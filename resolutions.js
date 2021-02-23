49fe96371187cc51cfb7ba3240c24e8b0908c577
rhiaro <amy@rhiaro.co.uk>
2021-02-23 14:30:54 +0000

    Init resolutions checker

---
+++ resolutions.js
@@ -0,0 +1,447 @@
+var data = [
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-02-09-did#resolution1",
+    "tags": [],
+    "done": false,
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-02-02-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-02-02-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-01-05-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-17-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-17-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-05-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-05-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-05-did#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-05-did#resolution4",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-05-did#resolution5",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-05-did#resolution6",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-03-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-03-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2010-11-02-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2010-11-02-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-10-06-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-22-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-15-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-15-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-11-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-11-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-07-01-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-07-01-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-06-30-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-06-30-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-06-09-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-05-19-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-21-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-21-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-21-did#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-21-did#resolution4",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-21-did#resolution5",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-21-did#resolution6",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-14-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-04-07-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-03-31-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-02-11-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-02-11-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-02-11-did#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-01-30-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-11-19-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-11-05-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-10-29-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-10-22-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-10-15-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-10-08-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-09-17-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-09-17-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-09-09-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-09-09-did#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-09-09-did#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-09-09-did#resolution4",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-09-09-did#resolution5",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-02-16-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-02-16-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-01-28-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-01-14-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-12-17-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-12-17-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-12-17-did-topic#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-12-17-did-topic#resolution4",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-19-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-11-19-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-29-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-29-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-29-did-topic#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-24-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-24-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-24-did-topic#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-09-24-did-topic#resolution4",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-27-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-27-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-27-did-topic#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-18-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-04-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-04-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-04-did-topic#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-04-did-topic#resolution4",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-04-did-topic#resolution5",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-08-04-did-topic#resolution6",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-07-21-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-07-21-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-05-12-did-topic#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-05-12-did-topic#resolution2",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-05-12-did-topic#resolution3",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2020-03-16-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-12-04-did#resolution1",
+    "tags": [],
+    "done": false
+},
+{
+    "uri": "https://www.w3.org/2019/did-wg/Meetings/Minutes/2019-12-04-did#resolution2",
+    "tags": [],
+    "done": false
+}
+];
